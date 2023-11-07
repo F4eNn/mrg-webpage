a933369609bff58044855bf3cc7d98cfbb70b798
Mateusz <mateusz4k@outlook.com>
2023-11-07 19:46:05 +0100

    chore: Provided some visual and cleanup changes, nothing big

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,21 +4,22 @@ import './globals.css';
 import type { Metadata } from 'next';
 import { Roboto } from 'next/font/google';
 
-import { Nav } from '@/components/nav/Nav'; 
+import { Header } from '@/components/header/Header';
 
 const roboto = Roboto({ subsets: ['latin'], weight: ['100', '300', '400', '500', '700'] });
 
 export const metadata: Metadata = {
-	title: 'Młodzieżowa Rada Gminy ',
-	description: 'Portal młodzieżowy',
+   title: 'Młodzieżowa Rada Gminy ',
+   description: 'Portal młodzieżowy',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-	return (
-		<html lang='en'>
-			<body className={roboto.className}>
-				<Nav />
-				{children}</body>
-		</html>
-	);
+   return (
+      <html lang='pl-PL'>
+         <body className={roboto.className}>
+            <Header />
+            {children}
+         </body>
+      </html>
+   );
 }
--- src/components/ui/Wrapper.tsx
+++ src/components/ui/Wrapper.tsx
@@ -1,5 +1,5 @@
 import React, { PropsWithChildren } from 'react';
 
 export const Wrapper = ({ children }: PropsWithChildren) => {
-return <div className='relative z-[20] mx-auto w-full max-w-[1440px]'>{children}</div>;
+   return <div className='relative z-[20] mx-auto w-full max-w-[1440px] px-3'>{children}</div>;
 };
