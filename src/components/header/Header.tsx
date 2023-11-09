0fc1cf780f2e9d8ceeb70ad8b229b0a54544ec86
Mateusz <mateusz4k@outlook.com>
2023-11-09 10:58:21 +0100

    refactor: changed html structure

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -3,25 +3,31 @@ import React from 'react';
 import { usePathname } from 'next/navigation';
 
 import { DesktopNav } from './DesktopNav';
-import { Wrapper } from '../ui/Wrapper';
 import { Backdrop } from '../ui/Backdrop';
 import { navigationArray } from '../../constants/navigation';
+import { MobileNav } from './MobileNav';
 
 export const Header = () => {
    const pathname = usePathname();
    if (!navigationArray.some(({ path }) => path === pathname)) return;
    return (
-      <header className='relative h-[80vh]'>
+      <header className='relative h-[100svh] overflow-hidden'>
          <div>
-            <video className='absolute inset-0 -z-10 h-full w-full bg-blue-500/70 object-cover'>
+            <video
+               autoPlay
+               loop
+               muted
+               playsInline
+               className='absolute inset-0 -z-10 h-full w-full bg-blue-500/70 object-cover'
+            >
                <source src='./hero.mp4' />
             </video>
             <Backdrop />
          </div>
-         <Wrapper>
+         <nav>
             <DesktopNav />
-            <div className='mt-5 h-[1px] w-full bg-white' />
-         </Wrapper>
+            <MobileNav />
+         </nav>
       </header>
    );
 };
