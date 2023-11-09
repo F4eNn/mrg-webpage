c5c72c71d32ef78ce4a4c5a0494b88bc79a9a791
Mateusz <mateusz4k@outlook.com>
2023-11-09 17:44:25 +0100

    refactor: provided children props instead title

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -1,15 +1,16 @@
-import { AnchorHTMLAttributes } from 'react';
+import { AnchorHTMLAttributes, PropsWithChildren } from 'react';
 import Link, { LinkProps } from 'next/link';
 
 import { cn } from '@/utils/cn';
 
 type LinkButtonProps = Pick<LinkProps, 'href'> &
-   AnchorHTMLAttributes<HTMLAnchorElement> & { title: string; className: string };
+   AnchorHTMLAttributes<HTMLAnchorElement> &
+   PropsWithChildren & { className?: string };
 
-export const LinkButton = ({ title, className, ...rest }: LinkButtonProps) => {
+export const LinkButton = ({ children, className, ...rest }: LinkButtonProps) => {
    return (
       <Link {...rest} className={cn('colors-300 rounded-lg p-3 ', className)}>
-         {title}
+         {children}
       </Link>
    );
 };
