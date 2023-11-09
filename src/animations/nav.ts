364823e2bec3794a8bae8c1bba8a68dd3376abc1
Mateusz <mateusz4k@outlook.com>
2023-11-09 10:59:57 +0100

    feat: created new animation for showing the active nav items

--- src/animations/nav.ts
+++ src/animations/nav.ts
@@ -5,6 +5,17 @@ export const desktopAnimation = (idx: number | undefined = 5) => ({
    animate: { x: 0, opacity: 1, transition: { delay: idx * 0.2, duration: 0.7 } },
 });
 
+export const activeDesktopPathAnimation: MotionProps = {
+   initial: { opacity: 0, height: 0, width: 0 },
+   animate: {
+      opacity: 1,
+      height: '100%',
+      width: '100%',
+      transition: { type: 'spring', stiffness: 95 },
+   },
+   exit: { opacity: 0, height: 0, width: 0 },
+};
+
 export const dropdownAnimation: MotionProps = {
    initial: { opacity: 0 },
    animate: { opacity: 1 },
