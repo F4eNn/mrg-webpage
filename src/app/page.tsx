3ba1d68ee8ffe7edd42d8e84d06f4dfba3013297
Mateusz <mateusz4k@outlook.com>
2023-11-12 11:44:06 +0100

    feat: added PreLoader component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import { Header } from '@/components/home/Header';
+import { PreLoader } from '@/components/ui/PreLoader';
 
 export default function Home() {
    return (
       <>
          <Header />
          <main className='h-[2000px]'>
+            <PreLoader />
             <p>MAIN</p>
          </main>
       </>
