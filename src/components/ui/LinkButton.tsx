5fb8e7970168b17e3579dbd96f5c9376807ccd3a
Mateusz <mateusz4k@outlook.com>
2023-11-14 17:13:12 +0100

    feat: added hover effect for linkButton and create tap animation

--- src/components/ui/LinkButton.tsx
+++ src/components/ui/LinkButton.tsx
@@ -1,7 +1,10 @@
+'use client';
 import { AnchorHTMLAttributes, PropsWithChildren } from 'react';
 import Link, { LinkProps } from 'next/link';
+import { motion } from 'framer-motion';
 
 import { cn } from '@/utils/cn';
+import { tapAnimate } from '@/animations/general';
 
 type LinkButtonProps = Pick<LinkProps, 'href'> &
    AnchorHTMLAttributes<HTMLAnchorElement> &
@@ -9,8 +12,16 @@ type LinkButtonProps = Pick<LinkProps, 'href'> &
 
 export const LinkButton = ({ children, className, ...rest }: LinkButtonProps) => {
    return (
-      <Link {...rest} className={cn('colors-300 rounded-[10px] p-3 ', className)}>
-         {children}
-      </Link>
+      <motion.div {...tapAnimate} className='flex items-center justify-center'>
+         <Link
+            {...rest}
+            className={cn(
+               " colors-300 relative z-0 overflow-hidden rounded-[10px] border-[1px] border-yellow bg-transparent p-3  after:absolute after:bottom-0 after:left-0 after:-z-10 after:h-0 after:w-full after:bg-white after:transition-all after:duration-500 after:content-[''] hover:border-white hover:text-lightblack hover:after:h-full",
+               className,
+            )}
+         >
+            {children}
+         </Link>
+      </motion.div>
    );
 };
