9e4422cd51f3b6c877433b4f61b911454a0e24a9
Mateusz <mateusz4k@outlook.com>
2023-11-16 20:28:36 +0100

    refactor: added cn utility for backdrop component

--- src/components/ui/Backdrop.tsx
+++ src/components/ui/Backdrop.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
-export const Backdrop = () => {
-    return <div className='absolute inset-0 -z-10 bg-black/70' />;
+import { cn } from '@/utils/cn';
+
+type BackdropProps = {
+   className?: string;
+};
+
+export const Backdrop = ({ className }: BackdropProps) => {
+   return <div className={cn('absolute inset-0 -z-10 bg-black/70', className)} />;
 };
