5164625e3a484aa330ef68b9af309372ae657d0f
Mateusz <mateusz4k@outlook.com>
2023-11-19 14:02:45 +0100

    refactor: changed types and fetch only need data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,33 +3,35 @@ import { PreLoader } from '@/components/ui/PreLoader';
 import { Greeting } from '@/components/home/Greeting';
 import { WeekReview } from '@/components/home/WeekReview';
 import { ProjectStatistics } from '@/components/home/ProjectStatistics';
-import { BACKEND_URL, API_TOKEN } from '@/constants/config';
+import { BACKEND_URL_API, API_TOKEN } from '@/constants/config';
 
-export interface IResponseData {
+export interface RootData {
    id: number;
-   attributes: {
-      tytul: string;
-      krotki_opis: string;
-      slug: string;
-      publishedAt: string;
-      zdjecie_glowne: {
-         data: {
-            id: number;
-            attributes: {
-               alternativeText: string;
-               hash: string;
-               url: string;
-            };
+   attributes: IPartialArticleData;
+}
+
+export interface IPartialArticleData {
+   tytul: string;
+   krotki_opis: string;
+   slug: string;
+   publishedAt: string;
+   zdjecie_glowne: {
+      data: {
+         id: number;
+         attributes: {
+            alternativeText: string;
+            hash: string;
+            url: string;
          };
       };
    };
 }
 
 const getArticles = async () => {
-   const URL = `${BACKEND_URL}/home-posts?sort[0]=id:asc&populate[zdjecie_glowne][fields][0]=url&populate[zdjecie_glowne][fields][1]=alternativeText&populate[zdjecie_glowne][fields][2]=hash&fields[0]=tytul&fields[1]=krotki_opis&fields[2]=slug&fields[3]=publishedAt&pagination[pageSize]=7`;
+   const URL = `${BACKEND_URL_API}/home-posts?sort[0]=id:desc&populate[zdjecie_glowne][fields][0]=url&populate[zdjecie_glowne][fields][1]=alternativeText&populate[zdjecie_glowne][fields][2]=hash&fields[0]=tytul&fields[1]=krotki_opis&fields[2]=slug&fields[3]=publishedAt&pagination[pageSize]=7`;
 
    const res = await fetch(URL, { cache: 'no-cache', headers: { Authorization: `Bearer ${API_TOKEN}` } });
-   const { data }: { data: IResponseData[] } = await res.json();
+   const { data }: { data: RootData[] } = await res.json();
    return data;
 };
 
@@ -41,7 +43,7 @@ export default async function Home() {
          <main>
             <PreLoader />
             <Greeting />
-            <WeekReview />
+            <WeekReview articleData={data} />
             <ProjectStatistics />
          </main>
       </>
