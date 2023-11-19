b59e20bff4f05b39292412029982d01286d14b59
Mateusz <mateusz4k@outlook.com>
2023-11-19 14:03:40 +0100

    feat: configure credentials config

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,4 @@
 
-export const URL = process.env.NEXT_STRAPI_URL || 'http://localhost:1337/api'
+export const BACKEND_URL_API = process.env.NEXT_STRAPI_URL_API_ENDPOINT 
+export const BACKEND_URL = process.env.NEXT_STRAPI_URL 
+export const API_TOKEN = process.env.NEXT_API_TOKEN
