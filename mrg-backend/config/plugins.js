3218122759a2c2e2b2dcee4988f268e1d74101fa
Mateusz <mateusz4k@outlook.com>
2023-11-20 15:08:12 +0100

    feat: added new backend dir

---
+++ mrg-backend/config/plugins.js
@@ -0,0 +1,14 @@
+
+module.exports = ({ env }) => ({
+  slugify: {
+    enabled: true,
+    config: {
+      contentTypes: {
+        'home-post': {
+          field: 'slug',
+          references: 'tytul',
+        },
+      },
+    },
+  },
+});
