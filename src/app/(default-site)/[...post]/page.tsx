e04d16b5fa31bb6020448f833dd070d052473eef
Mateusz <mateusz4k@outlook.com>
2023-11-23 14:47:03 +0100

    feat: fetching data and handling errors from cms

--- src/app/(default-site)/[...post]/page.tsx
+++ src/app/(default-site)/[...post]/page.tsx
@@ -1,17 +1,73 @@
 import React from 'react';
+import { notFound } from 'next/navigation';
 
 import { PostHeader } from '@/components/home/post/Header';
 import { Wrapper } from '@/components/ui/Wrapper';
 import { PostContent } from '@/components/home/post/Content';
 import { PostGallery } from '@/components/home/post/Gallery';
 import { Aside } from '@/components/home/post/Aside';
+import { API_TOKEN, BACKEND_URL_API } from '@/constants/config';
+import { getErrorMessage } from '@/utils/getErrorMessage';
 
-const PostPage = () => {
+import { RootDataType } from '../page';
+
+export interface IPostData {
+   zawartosc_posta: string;
+   tytul: string;
+   publishedAt: string;
+   zdjecie_glowne: {
+      data: {
+         attributes: {
+            url: string;
+            hash: string;
+            alt: string;
+            width: number;
+            height: number;
+         };
+      };
+   };
+}
+
+const getPost = async (param: string) => {
+   try {
+      const res = await fetch(`${BACKEND_URL_API}/slugify/slugs/home-post/${param}?populate=*`, {
+         cache: 'no-cache',
+         headers: { Authorization: `Bearer ${API_TOKEN}` },
+      });
+      if (res.status === 404) {
+         throw new Error('404');
+      }
+      if (res.status === 403 || res.status === 500 || res.status === 401) {
+         throw new Error('Wykryto błąd po stronie serwera, spróbuj ponownie lub wróć do strony głównej.');
+      }
+      if (!res.ok) {
+         throw new Error('Wykryto nieoczekiwany błąd, spróbuj ponownie lub wróć do strony głównej.');
+      }
+      const { data }: { data: RootDataType<IPostData> } = await res.json();
+      return data;
+   } catch (err: unknown) {
+      return {
+         errMsg: getErrorMessage(err),
+      };
+   }
+};
+
+const PostPage = async ({ params }: { params: { post: string[] } }) => {
+   const data = await getPost(params.post[1]);
+
+   if ('errMsg' in data) {
+      if (data.errMsg === '404') {
+         notFound();
+      }
+      throw new Error(data.errMsg);
+   }
+   const { publishedAt, tytul, zawartosc_posta, zdjecie_glowne } = data.attributes;
+
    return (
       <main className='mb-32'>
          <Wrapper className='grid grid-cols-2 gap-10 lg:grid-cols-4'>
             <article className='col-span-3 space-y-16 '>
-               <PostHeader />
+               <PostHeader publishedAt={publishedAt} title={tytul} main_picture={zdjecie_glowne} />
                <PostContent />
                <PostGallery />
             </article>
