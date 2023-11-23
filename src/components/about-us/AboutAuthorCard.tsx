eae5f4e38ff975795e622227eb53394180c78bba
Mateusz <mateusz4k@outlook.com>
2023-11-23 20:01:41 +0100

    feat: added new author to about-us page

--- src/components/about-us/AboutAuthorCard.tsx
+++ src/components/about-us/AboutAuthorCard.tsx
@@ -8,30 +8,28 @@ import { IAboutAuthorCard } from '@/components/about-us/about-us-data';
 
 type AboutAuthorsProps = IAboutAuthorCard & { idx: number };
 
-export const AboutAuthorCard = ({ name, imageSrc, staticImg, text, text2, text3, idx, bg }: AboutAuthorsProps) => {
+export const AboutAuthorCard = ({ name,staticImg, text, text2, text3, idx, bg }: AboutAuthorsProps) => {
    return (
-      <article className={`my-24 ${bg} py-10 lg:my-32`}>
-         <Wrapper className='grid grid-cols-1 gap-20  lg:grid-cols-2'>
-            <div className={`${idx % 2 !== 1 ? 'order-1' : ''}`}>
-               <Heading className='italic text-white lg:text-left' as='h2' title={name} />
-               <div className='mt-20 space-y-10  text-white'>
+      <article className={`my-24 ${bg} py-20 lg:py-10 lg:my-32`}>
+         <Wrapper className='grid grid-cols-1 gap-14 lg:gap-20   lg:grid-cols-2'>
+            <div className={`${idx % 2 !== 1 ? 'lg:order-1 order-last ' : 'max-lg:order-last'}`}>
+               <Heading className='italic text-white lg:text-left ' as='h2' title={name} />
+               <div className='mt-10 lg:mt-20 space-y-10  text-white'>
                   <TextDesc className='lg:text-left'>{text}</TextDesc>
                   <TextDesc className='lg:text-left'>{text2}</TextDesc>
                   <TextDesc className='lg:text-left'>{text3}</TextDesc>
                </div>
             </div>
             <div className='overflow-hidden rounded-md'>
-               <picture>
-                  <source media='(max-width: 640px)' srcSet={imageSrc} />
-                  <Image
-                     src={staticImg}
-                     placeholder='blur'
-                     width={2048}
-                     height={2048}
-                     alt='Łukasz Kołodziej'
-                     className='h-full w-full object-cover'
-                  />
-               </picture>
+               <Image
+                  src={staticImg}
+                  placeholder='blur'
+                  width={2048}
+                  height={2048}
+                  alt='Łukasz Kołodziej'
+                  className='h-full w-full max-h-[600px] object-cover'
+                  sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 70vw'
+               />
             </div>
          </Wrapper>
       </article>
--- src/components/about-us/about-us-data.ts
+++ src/components/about-us/about-us-data.ts
@@ -1,44 +1,31 @@
 import { StaticImageData } from 'next/image';
 
 import lukaszImg from '../../../public/assets/o-nas/lukasz-kolodziej.webp';
+import kamilImg from '../../../public/assets/o-nas/Kamil.jpg';
 
 export interface IAboutAuthorCard {
    name: string;
    text: string;
    text2?: string;
    text3?: string;
-   imageSrc: string;
    staticImg: StaticImageData;
-   bg: string
+   bg: string;
 }
 
 export const authors: IAboutAuthorCard[] = [
    {
       name: 'Łukasz Kołodziej',
-      imageSrc: '/assets/o-nas/lukasz-kolodziej-mobile.webp',
       staticImg: lukaszImg,
       text: ' Redaktor naczelny, student prawa, doświadczony działacz społeczny, Przewodniczący III kadencji Młodzieżowej Rady Gminy Kościelisko. Lubi dowodzić swoich racji choć kiedy trzeba idzie na kompromis. Mieszkaniec gminy Kościelisko, od 4 lat uczestniczy w jej życiu publicznym, z nią wiąże swoją przyszłość.',
       text2: ' Jego głównym zajęciem poza studiami jest praca z młodzieżą, a robi to m.in. Opiekując się Młodzieżową Radą Gminy Kościelisko, która jest jego oczkiem w głowie. Łukasz to człowiek, który ma więcej zainteresowań niż lat, a jednak wciąż próbuje i uczy się nowych rzeczy.',
       text3: '  Europa nie ma dla niego granic, ciągle odkrywa ją dzięki uczestnictwu w międzynarodowych projektach adresowanych do osób pracujących z młodzieżą. Przy okazji rozwija swoje kompetencje miękkie, bez których - jak twierdzi - trudno funkcjonować we współczesnym świecie.',
-      bg: 'bg-[#f39f5a]'
+      bg: 'bg-[#f39f5a]',
    },
    {
       name: 'Kamil Szynal',
-      imageSrc: '/assets/o-nas/lukasz-kolodziej-mobile.webp',
-      staticImg: lukaszImg,
-      text: ' Redaktor naczelny, student prawa, doświadczony działacz społeczny, Przewodniczący III kadencji Młodzieżowej Rady Gminy Kościelisko. Lubi dowodzić swoich racji choć kiedy trzeba idzie na kompromis. Mieszkaniec gminy Kościelisko, od 4 lat uczestniczy w jej życiu publicznym, z nią wiąże swoją przyszłość.',
-      text2: ' Jego głównym zajęciem poza studiami jest praca z młodzieżą, a robi to m.in. Opiekując się Młodzieżową Radą Gminy Kościelisko, która jest jego oczkiem w głowie. Łukasz to człowiek, który ma więcej zainteresowań niż lat, a jednak wciąż próbuje i uczy się nowych rzeczy.',
-    
-      bg: 'bg-[#662549]'
+      staticImg: kamilImg,
+      text: 'Członek zarządu, zajmuje się rekonstrukcją historyczną, poszukiwaniami historycznymi oraz mediami i dziennikarstwem, a także prowadzi bloga. Uczył się w Zespole Szkół im. Hugona Kołłątaja w Jordanowie, a obecnie studiuje historię w Instytucie Historii Wojskowej Akademii Sztuki Wojennej w Warszawie.',
+      text3: ' Stara się pomagać innym oraz promować pozytywne wartości oraz wyciągać wnioski z przeszłości i przedstawiać je, łącznie z prawdziwą historią - na podstawie badań źródłowych. A przy okazji realizuje swoje własne cele i marzenia.',
+      bg: 'bg-[#662549]',
    },
-   {
-      name: 'Łukasz Kołodziej',
-      imageSrc: '/assets/o-nas/lukasz-kolodziej-mobile.webp',
-      staticImg: lukaszImg,
-      text: ' Redaktor naczelny, student prawa, doświadczony działacz społeczny, Przewodniczący III kadencji Młodzieżowej Rady Gminy Kościelisko. Lubi dowodzić swoich racji choć kiedy trzeba idzie na kompromis. Mieszkaniec gminy Kościelisko, od 4 lat uczestniczy w jej życiu publicznym, z nią wiąże swoją przyszłość.',
-      text2: ' Jego głównym zajęciem poza studiami jest praca z młodzieżą, a robi to m.in. Opiekując się Młodzieżową Radą Gminy Kościelisko, która jest jego oczkiem w głowie. Łukasz to człowiek, który ma więcej zainteresowań niż lat, a jednak wciąż próbuje i uczy się nowych rzeczy.',
-      text3: '  Europa nie ma dla niego granic, ciągle odkrywa ją dzięki uczestnictwu w międzynarodowych projektach adresowanych do osób pracujących z młodzieżą. Przy okazji rozwija swoje kompetencje miękkie, bez których - jak twierdzi - trudno funkcjonować we współczesnym świecie.',
-      bg: 'bg-[#ae445a]'
-   },
-
 ];
