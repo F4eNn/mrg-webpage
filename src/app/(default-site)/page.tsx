e87a063ea0ea76050dda3cc9620a1748cf7a4fac
Mateusz <mateusz4k@outlook.com>
2023-11-23 14:50:22 +0100

    refactor: made RootDataType as a generic

--- src/app/(default-site)/page.tsx
+++ src/app/(default-site)/page.tsx
@@ -5,9 +5,9 @@ import { WeekReview } from '@/components/home/WeekReview';
 import { ProjectStatistics } from '@/components/home/ProjectStatistics';
 import { BACKEND_URL_API, API_TOKEN } from '@/constants/config';
 
-export interface RootData {
+export interface RootDataType<T> {
    id: number;
-   attributes: IPartialArticleData;
+   attributes: T;
 }
 
 export interface IPartialArticleData {
@@ -31,7 +31,7 @@ const getArticles = async () => {
    const URL = `${BACKEND_URL_API}/home-posts?sort[0]=id:desc&populate[zdjecie_glowne][fields][0]=url&populate[zdjecie_glowne][fields][1]=alternativeText&populate[zdjecie_glowne][fields][2]=hash&fields[0]=tytul&fields[1]=krotki_opis&fields[2]=slug&fields[3]=publishedAt&pagination[pageSize]=7`;
 
    const res = await fetch(URL, { cache: 'no-cache', headers: { Authorization: `Bearer ${API_TOKEN}` } });
-   const { data }: { data: RootData[] } = await res.json();
+   const { data }: { data: RootDataType<IPartialArticleData>[] } = await res.json();
    return data;
 };
 
