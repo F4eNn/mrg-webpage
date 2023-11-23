315e73f303279314d5d84fe9c4f6173006a0aeff
Mateusz <mateusz4k@outlook.com>
2023-11-23 14:51:38 +0100

    feat: created an error page to handle  diff type of errors

---
+++ src/app/error.tsx
@@ -0,0 +1,33 @@
+'use client';
+import Link from 'next/link';
+
+import { Button, buttonVariants } from '@/components/controls/Button';
+import { Heading } from '@/components/ui/Heading';
+import { Wrapper } from '@/components/ui/Wrapper';
+import { navigationPaths } from '@/constants/navigation';
+
+export default function Error({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+   return (
+      <html>
+         <body>
+            <main className='h-screen bg-white pt-10'>
+               <Wrapper className='flex flex-col items-center gap-10'>
+                  <Heading as='h3' className='max-w-[600px]' title={`Oops! ${error.message}`} />
+                  <div className='space-x-10'>
+                     <Button variant={'secondary'} size={'default'} onClick={() => reset()}>
+                        Spróbuj ponownie
+                     </Button>
+                     <Link
+                        href={navigationPaths.home.path}
+                        className={buttonVariants({ size: 'default', variant: 'secondary' })}
+                        onClick={() => reset()}
+                     >
+                        Home
+                     </Link>
+                  </div>
+               </Wrapper>
+            </main>
+         </body>
+      </html>
+   );
+}
