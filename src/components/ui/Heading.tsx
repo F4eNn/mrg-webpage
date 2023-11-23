478538d52917a09a2eea9887f32e7a10717f1988
Mateusz <mateusz4k@outlook.com>
2023-11-23 19:39:28 +0100

    feat: added possibility to pass children

--- src/components/ui/Heading.tsx
+++ src/components/ui/Heading.tsx
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { PropsWithChildren } from 'react';
 
 import { cn } from '@/utils/cn';
 
-type HeadingProps = {
+type HeadingProps = PropsWithChildren & {
    as: 'h1' | 'h2' | 'h3';
-   title: string;
+   title?: string;
    className?: string;
 };
 
-export const Heading = ({ as, title, className }: HeadingProps) => {
+export const Heading = ({ as, title, children, className }: HeadingProps) => {
    if (as === 'h2') {
-      return <h2 className={cn('text-center text-xl font-bold phones:text-3xl md:text-3xl text-lightblack', className)}>{title}</h2>;
+      return (
+         <h2 className={cn('text-center text-xl font-bold text-lightblack phones:text-3xl md:text-3xl', className)}>
+            {title || children}
+         </h2>
+      );
    } else if (as === 'h3') {
-      return <h3 className={cn('text-lg text-center font-bold phones:text-2xl text-lightblack', className)}>{title}</h3>;
+      return (
+         <h3 className={cn('text-center text-lg font-bold text-lightblack phones:text-2xl', className)}>
+            {title || children}
+         </h3>
+      );
    }
-   return <h1 className={cn('text-xl font-bold text-center phones:text-3xl md:text-4xl text-lightblack', className)}>{title}</h1>;
+   return (
+      <h1 className={cn('text-center text-xl font-bold text-lightblack phones:text-3xl md:text-4xl', className)}>
+         {title || children}
+      </h1>
+   );
 };
