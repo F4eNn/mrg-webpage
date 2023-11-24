ec84a74b3dceef0863cfcf18f303d3145cca79b2
Mateusz <mateusz4k@outlook.com>
2023-11-24 10:52:34 +0100

    installed Plaiceholder for images and create fn that convert single media to base64

---
+++ next.config.mjs
@@ -0,0 +1,24 @@
+import withPlaiceholder from '@plaiceholder/next';
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+   webpack(config) {
+      config.module.rules.push({
+         test: /\.svg$/,
+         use: ['@svgr/webpack'],
+      });
+
+      return config;
+   },
+   images: {
+      remotePatterns: [
+         { hostname: 'localhost', port: '1337', pathname: '/uploads/**', protocol: 'http' },
+         { hostname: 'res.cloudinary.com', pathname: `/${process.env.NEXT_CLOUDINARY_NAME}/**`, protocol: 'https' },
+      ],
+   },
+   experimental: {
+      webpackBuildWorker: true,
+   },
+};
+
+export default withPlaiceholder(nextConfig);
---
+++ src/utils/blurDataUrl.ts
@@ -0,0 +1,17 @@
+import { getPlaiceholder } from 'plaiceholder';
+
+export const getBase64 = async (image: string) => {
+   try {
+      const res = await fetch(image);
+      if (!res.ok) {
+         throw new Error(`Failed to fetch image ${res.status} ${res.statusText}`);
+      }
+      const buffer = await res.arrayBuffer();
+
+      const { base64 } = await getPlaiceholder(Buffer.from(buffer));
+      return base64;
+      
+   } catch (error) {
+      console.error(error);
+   }
+};
