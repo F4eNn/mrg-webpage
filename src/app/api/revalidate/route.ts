f4471c43602f96cef1a6897e9059cc66934e5e9c
Mateusz <mateusz4k@outlook.com>
2023-11-27 15:16:48 +0100

    feat: created path revalidation function

---
+++ src/app/api/revalidate/route.ts
@@ -0,0 +1,21 @@
+import { revalidatePath } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+   const path = request.nextUrl.searchParams.get('path');
+   const secureToken = request.nextUrl.searchParams.get('secret');
+   if (secureToken !== process.env.NEXT_REVALIDATE_TOKEN) {
+      return new NextResponse(JSON.stringify('Invalid token'), {
+         status: 401,
+         statusText: 'Unauthorized',
+         headers: {
+            'Content-Type': 'application/json',
+         },
+      });
+   }
+   if (path) {
+      revalidatePath(path);
+      return NextResponse.json({ revalidated: true });
+   }
+   return NextResponse.json({ revalidated: false, message: 'Missing path to revalidate' });
+}
