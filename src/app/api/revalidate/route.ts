34d400e3d8b3dd99e3cc06f931f86bab33941990
Mateusz <mateusz4k@outlook.com>
2023-11-28 10:29:07 +0100

    fix: issue in api revalidate, provided argument - what exacly want to revalidate

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,9 +1,10 @@
-import { revalidatePath } from 'next/cache';
 import { NextRequest, NextResponse } from 'next/server';
+import { revalidatePath } from 'next/cache';
 
 export async function POST(request: NextRequest) {
    const path = request.nextUrl.searchParams.get('path');
    const secureToken = request.nextUrl.searchParams.get('secret');
+
    if (secureToken !== process.env.NEXT_REVALIDATE_TOKEN) {
       return new NextResponse(JSON.stringify('Invalid token'), {
          status: 401,
@@ -14,7 +15,7 @@ export async function POST(request: NextRequest) {
       });
    }
    if (path) {
-      revalidatePath(path);
+      revalidatePath(path, 'page');
       return NextResponse.json({ revalidated: true });
    }
    return NextResponse.json({ revalidated: false, message: 'Missing path to revalidate' });
