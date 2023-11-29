10ebb8400e69ef3826def0f78d8dc1d36ad402f6
Mateusz <mateusz4k@outlook.com>
2023-11-29 18:24:15 +0100

    chore: changed env var to public

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,2 +1,2 @@
-export const BACKEND_URL_API = process.env.NEXT_STRAPI_URL_API_ENDPOINT;
-export const API_TOKEN = process.env.NEXT_API_TOKEN;
+export const BACKEND_URL_API = process.env.NEXT_PUBLIC_STRAPI_URL_API_ENDPOINT;
+export const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
