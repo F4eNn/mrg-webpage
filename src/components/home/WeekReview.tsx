faf5d8eac116bb0a74a6662f4e5feafe8f784b71
Mateusz <mateusz4k@outlook.com>
2023-11-29 18:20:00 +0100

    refactor: removed api fetch from entire home page and move it inside weekReview, making the  component asynchronous

--- src/components/home/WeekReview.tsx
+++ src/components/home/WeekReview.tsx
@@ -3,14 +3,48 @@ import Link from 'next/link';
 import Image from 'next/image';
 
 import { Wrapper } from '@/components/ui/Wrapper';
-import { IPartialArticleData, RootDataType } from '@/app/(default-site)/page';
+import { fetchAPI } from '@/utils/fetch-api';
+import { IFormatType } from '@/types/model';
 
 import { ArticleCard } from '../ui/ArticleCard';
 import { Section } from '../ui/Section';
 import { Heading } from '../ui/Heading';
 import { Backdrop } from '../ui/Backdrop';
 
-export const WeekReview = ({ articleData }: { articleData: RootDataType<IPartialArticleData>[] }) => {
+export interface RootDataType<T> {
+   id: number;
+   attributes: T;
+}
+
+export interface IPartialArticleData {
+   tytul: string;
+   krotki_opis: string;
+   slug: string;
+   publishedAt: string;
+   zdjecie_glowne: {
+      data: {
+         id: number;
+         attributes: IFormatType;
+      };
+   };
+}
+
+export const WeekReview = async () => {
+   const path = '/home-posts';
+   const urlParamsObject = {
+      sort: ['publishedAt:desc'],
+      populate: {
+         zdjecie_glowne: { fields: ['formats'] },
+      },
+      fields: ['tytul', 'krotki_opis', 'slug', 'publishedAt'],
+      pagination: { pageSize: 7 },
+   };
+
+   const { data } = await fetchAPI<RootDataType<IPartialArticleData>[]>(path, urlParamsObject);
+
+   if (!data) {
+      return <p className='text-center'>Coś poszło nie tak, spróbuj ponownie później.</p>;
+   }
    return (
       <Section id='przeglad-tygodnia'>
          <Wrapper>
@@ -18,19 +52,19 @@ export const WeekReview = ({ articleData }: { articleData: RootDataType<IPartial
          </Wrapper>
          <Wrapper className='grid gap-14 xl:gap-20 '>
             <div className='col-span-2 grid gap-14 md:grid-cols-2 xl:gap-20'>
-               {articleData.map(({ attributes, id }, idx) => {
+               {data.map(({ attributes, id }, idx) => {
                   if (idx <= 1) {
                      return <ArticleCard key={id} articleData={attributes} />;
                   }
                })}
             </div>
             <div className=' col-span-2 grid grid-cols-1 gap-14 md:grid-cols-2 lg:grid-cols-3 xl:gap-20'>
-               {articleData.map(({ attributes, id }, idx) => {
+               {data.map(({ attributes, id }, idx) => {
                   if (idx > 1) {
                      return <ArticleCard key={id} variant={'fresh'} articleData={attributes} />;
                   }
                })}
-               <Link href='' className='transform-300 relative hover:scale-[1.02] active:scale-100'>
+               <Link href='/' className='transform-300 relative hover:scale-[1.02] active:scale-100'>
                   <Image
                      src='/assets/home/blured-article.webp'
                      alt=''
