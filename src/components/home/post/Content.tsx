af641194ac84d6baa28c04dcb0cdb2c70a6238ba
Mateusz <mateusz4k@outlook.com>
2023-11-29 20:21:24 +0100

    chore: minor styles changes

--- src/components/home/post/Content.tsx
+++ src/components/home/post/Content.tsx
@@ -46,8 +46,8 @@ export const PostContent = ({ content }: IPostContentProps) => {
                         alt={alt}
                         width={900}
                         height={600}
-                        className='max-h-[600px] w-full mx-auto object-contain rounded-sm'
-                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 80vw'
+                        className='max-h-[500px] w-full mx-auto object-cover rounded-sm'
+                        sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 95vw'
                      />
                   );
                case 'a':
