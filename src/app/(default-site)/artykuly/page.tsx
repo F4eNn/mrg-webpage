5b93807e7b11d059a1dded1a3ecfe56e9f686932
Mateusz <mateusz4k@outlook.com>
2023-12-05 12:00:48 +0100

    refactor: put content into wrapper

--- src/app/(default-site)/artykuly/page.tsx
+++ src/app/(default-site)/artykuly/page.tsx
@@ -4,6 +4,7 @@ import { IFormatType, IMetaStrapi, RootDataType } from '@/types/model';
 import { Section } from '@/components/ui/Section';
 import { Heading } from '@/components/ui/Heading';
 import { LoadArticles } from '@/components/articles/LoadArticles';
+import { Wrapper } from '@/components/ui/Wrapper';
 
 export interface ArticlesData {
    tytul: string;
@@ -22,9 +23,9 @@ const page = () => {
       <main>
          <Section>
             <Heading as='h1' title='Starsze Artykuły' className='mb-20 mt-32 md:mb-36 md:mt-48' />
-            <div className=' grid gap-x-16 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-y-24 px-5 2xl:gap-x-26 2xl:gap-y-28'>
+            <Wrapper className='2xl:gap-x-26  grid max-w-[2250px] gap-x-16 gap-y-24 px-5 md:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 2xl:gap-y-28'>
                <LoadArticles />
-            </div>
+            </Wrapper>
          </Section>
       </main>
    );
